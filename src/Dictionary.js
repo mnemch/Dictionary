f737b5badf615f2a0e587e96f61c40478bc11f84
mnemch <nemchinova.marya@gmail.com>
2021-05-09 12:33:45 +0200

    +photos w/o fix search

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 // let apikey from map = "eb1733f56daae1f8c299ea8db424ea64";
@@ -9,7 +10,7 @@ import "./Dictionary.css";
 
 export default function Dictionary(props) {
     let [searchWord, setSearchWord] = useState(props.defaultTerm);
-    // let [photos, setPhotos] = useState(null);
+    let [photos, setPhotos] = useState(null);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
 
@@ -21,24 +22,24 @@ export default function Dictionary(props) {
     setResults(response.data[0]);
     }
 
-    // function handPexelsResponse(response) {
-    // setPhotos(response.data.photos);
-    // }
+    function handPexelsResponse(response) {
+    setPhotos(response.data.photos);
+    }
 
     function search(event) {
-        event.preventDefault();
+        
         // alert(`you are searching for ${searchWord}`);
         // dictionary api documentation: https://dictionaryapi.dev/
         // https://api.dictionaryapi.dev/api/v2/entries/en_US/hello 
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`;
         axios.get(apiUrl).then(handResponse);
 
-        //  pexels api documentation: https://www.pexels.com/api/documentation/
-    //     let pexelsApiKey ="563492ad6f917000010000010c07d0b470024e508205a11acb696a0a";
-    //     let pexelsUrl = `https://api.pexels.com/v1/search?query=${searchWord}&per_page=2`;
-    // axios
-    //   .get(pexelsUrl, { headers: { Authorization: `Bearer ${pexelsApiKey}` } })
-    //   .then(handPexelsResponse);
+         // pexels api documentation: https://www.pexels.com/api/documentation/
+        let pexelsApiKey ="563492ad6f917000010000010c07d0b470024e508205a11acb696a0a";
+        let pexelsUrl = `https://api.pexels.com/v1/search?query=${searchWord}&per_page=2`;
+    axios
+      .get(pexelsUrl, { headers: { Authorization: `Bearer ${pexelsApiKey}` } })
+      .then(handPexelsResponse);
   }
     function load() {
     setLoaded(true);
@@ -53,9 +54,9 @@ export default function Dictionary(props) {
             <input type="search" onChange={handSearchChange} placeholder="Please enter your word here 🪄 "/>
             </form>
             </section>
-   
             <Results results={results}/>
-
+            <br/>
+            <Photos photos={photos} alt={searchWord}/>
             </div>
     );
   } else {
